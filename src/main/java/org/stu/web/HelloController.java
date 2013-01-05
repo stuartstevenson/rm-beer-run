ee26c724a11ac105fa0be5c7aac140fc73cad444
Stuart Stevenson <stuartjackstevenson@gmail.com>
2013-01-05 15:49:09 +0000

    hello world app

---
+++ src/main/java/org/stu/web/HelloController.java
@@ -0,0 +1,20 @@
+package org.stu.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tamsin
+ * Date: 05/01/13
+ * Time: 14:03
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+public class HelloController {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String showHello() {
+        return "hello";
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Tamsin
+  Date: 05/01/13
+  Time: 14:10
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    hello
+</body>
+</html>
