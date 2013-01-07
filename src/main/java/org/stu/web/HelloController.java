c4394eb9c023f082e2dfae53597c84fbd6a92c6b
stuartstevenson <stuartjackstevenson@gmail.com>
2013-01-07 15:30:38 +0000

    changing homepage path

--- src/main/java/org/stu/web/HelloController.java
+++ src/main/java/org/stu/web/HelloController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HelloController {
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String showHello() {
         return "hello";
     }
