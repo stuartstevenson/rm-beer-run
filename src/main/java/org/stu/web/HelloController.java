e5a8622eacbf8cb2e39bb95bb4f045efdc658074
Stuart Stevenson <stuartjackstevenson@gmail.com>
2013-02-18 20:02:03 +0000

    removing comments

--- src/main/java/org/stu/web/HelloController.java
+++ src/main/java/org/stu/web/HelloController.java
@@ -4,13 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Tamsin
- * Date: 05/01/13
- * Time: 14:03
- * To change this template use File | Settings | File Templates.
- */
 @Controller
 public class HelloController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
